2b0dae1c264a8c76c183957fe784ddf8f745f969
HaoCold <xjh093@126.com>
2018-07-19 11:05:54 +0800

    Update UIView+JHFrameLayout.h

--- JHFrameLayout/UIView+JHFrameLayout.h
+++ JHFrameLayout/UIView+JHFrameLayout.h
@@ -39,18 +39,21 @@
 
 // width
 - (void)jh_widthIs:(CGFloat)width;
+- (void)jh_widthIsEqualToView:(UIView *)view;
 - (void)jh_widthIs:(CGFloat)ratio ratioOfViewWidth:(UIView *)view;
 - (void)jh_widthIs:(CGFloat)ratio ratioOfViewHeight:(UIView *)view;
 
 
 // height
 - (void)jh_heightIs:(CGFloat)height;
+- (void)jh_heightIsEqualToView:(UIView *)view;
 - (void)jh_heightIs:(CGFloat)ratio ratioOfViewWidth:(UIView *)view;
 - (void)jh_heightIs:(CGFloat)ratio ratioOfViewHeight:(UIView *)view;
 
 
 /// top space in super view
 - (void)jh_topIs:(CGFloat)top;
+- (void)jh_topIsEqualToView:(UIView *)view;
 // if flag is YES, will update height to keep bottom space
 - (void)jh_topIs:(CGFloat)offsetY fromTopOfView:(UIView *)view updateHeight:(BOOL)flag;
 - (void)jh_topIs:(CGFloat)offsetY fromMiddleOfView:(UIView *)view updateHeight:(BOOL)flag;
@@ -59,6 +62,7 @@
 
 /// left space in super view
 - (void)jh_leftIs:(CGFloat)left;
+- (void)jh_leftIsEqualToView:(UIView *)view;
 // if flag is YES, will update width to keep right space
 - (void)jh_leftIs:(CGFloat)offsetX fromLeftOfView:(UIView *)view updateWidth:(BOOL)flag;
 - (void)jh_leftIs:(CGFloat)offsetX fromMiddleOfView:(UIView *)view updateWidth:(BOOL)flag;
@@ -67,6 +71,7 @@
 
 /// bottom space in super view,if 'height' > 0, 'y' will be modified,otherwise modify 'height'.
 - (void)jh_bottomIs:(CGFloat)bottom;
+- (void)jh_bottomIsEqualToView:(UIView *)view;
 // if flag is YES, will update height to keep top space
 - (void)jh_bottomIs:(CGFloat)offsetY fromBottomOfView:(UIView *)view updateHeight:(BOOL)flag;
 - (void)jh_bottomIs:(CGFloat)offsetY fromMiddleOfView:(UIView *)view updateHeight:(BOOL)flag;
@@ -75,6 +80,7 @@
 
 /// right space to super view,if 'width' > 0, 'x' will be modified,otherwise modify 'width'.
 - (void)jh_rightIs:(CGFloat)right;
+- (void)jh_rightIsEqualToView:(UIView *)view;
 // if flag is YES, will update width to keep left space
 - (void)jh_rightIs:(CGFloat)offsetX fromRightOfView:(UIView *)view updateWidth:(BOOL)flag;
 - (void)jh_rightIs:(CGFloat)offsetX fromMiddleOfView:(UIView *)view updateWidth:(BOOL)flag;
@@ -83,6 +89,7 @@
 
 /// center X
 - (void)jh_centerXIs:(CGFloat)centerX;
+- (void)jh_centerXIsEqualToView:(UIView *)view;
 // if flag is YES, will update width, keep left space for right move, keep right space for left move
 - (void)jh_centerXIs:(CGFloat)offsetX fromLeftOfView:(UIView *)view updateWidth:(BOOL)flag;
 - (void)jh_centerXIs:(CGFloat)offsetX fromMiddleOfView:(UIView *)view updateWidth:(BOOL)flag;
@@ -90,6 +97,7 @@
 
 /// center Y
 - (void)jh_centerYIs:(CGFloat)centerY;
+- (void)jh_centerYIsEqualToView:(UIView *)view;
 // if flag is YES, wiil update height, keep top space for bottom move, keep bottom space for top move
 - (void)jh_centerYIs:(CGFloat)offsetY fromTopOfView:(UIView *)view updateHeight:(BOOL)flag;
 - (void)jh_centerYIs:(CGFloat)offsetY fromMiddleOfView:(UIView *)view updateHeight:(BOOL)flag;
